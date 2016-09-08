215014d0b5cd312459049ad321c8ba7e43ce2cd3
Li Jiang <mingwl@aliyun.com>
2016-09-08 18:22:26 +0800

    初始提交

---
+++ helloWorld/src/main/java/ming/wl/helloWorld/App.java
@@ -0,0 +1,13 @@
+package ming.wl.helloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
