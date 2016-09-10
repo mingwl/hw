f0063e1a8f4f007591abe618935d56341f297864
Li Jiang <mingwl@aliyun.com>
2016-09-11 04:03:04 +0800

    其他产生散列/哈希算法SHA256的方法

---
+++ helloWorld/src/main/java/ming/wl/helloWorld/SHAHashingExample.java
@@ -0,0 +1,34 @@
+package ming.wl.helloWorld;
+
+import java.security.MessageDigest;
+
+public class SHAHashingExample {
+
+	public static void main(String[] args)throws Exception
+  {
+//  	String password = "A";
+  	String password = "61cf3e27-f024-4f9d-af44-b41b6659b385";
+
+      MessageDigest md = MessageDigest.getInstance("SHA-512");
+      md.update(password.getBytes());
+
+      byte byteData[] = md.digest();
+
+      //convert the byte to hex format method 1
+      StringBuffer sb = new StringBuffer();
+      for (int i = 0; i < byteData.length; i++) {
+       sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+      }
+
+      System.out.println("Hex format : " + sb.toString());
+
+      //convert the byte to hex format method 2
+      StringBuffer hexString = new StringBuffer();
+  	for (int i=0;i<byteData.length;i++) {
+  		String hex=Integer.toHexString(0xff & byteData[i]);
+ 	     	if(hex.length()==1) hexString.append('0');
+ 	     	hexString.append(hex);
+  	}
+  	System.out.println("Hex format : " + hexString.toString());
+  }
+}
